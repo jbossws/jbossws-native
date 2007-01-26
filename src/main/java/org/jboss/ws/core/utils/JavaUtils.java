3f813beb61797c1902f68a319b1c936b971d3b13
jason.greene@jboss.com <jason.greene@jboss.com>
2007-01-26 20:49:22 +0000

    Revert 2118. There is no good reason to get rid of this routine!
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/trunk/jbossws-core@2143 50344849-fb0f-0410-a53f-a94597aac05c

--- src/main/java/org/jboss/ws/core/utils/JavaUtils.java
+++ src/main/java/org/jboss/ws/core/utils/JavaUtils.java
@@ -520,6 +520,20 @@ public class JavaUtils
       return c + source.substring(1);
    }
 
+   public static boolean isLoaded(String className, ClassLoader loader)
+   {
+      try
+      {
+         loadJavaType(className, loader);
+      }
+      catch (ClassNotFoundException e)
+      {
+         return false;
+      }
+
+      return true;
+   }
+
    public static String getPackageName(Class<?> clazz)
    {
       String fullName = clazz.getName();
