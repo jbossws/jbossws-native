2f5484e84f2d85f7c93aa3dfc877e9670970041a
jason.greene@jboss.com <jason.greene@jboss.com>
2007-01-31 19:44:24 +0000

    initial implementation of wsimport
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/trunk/jbossws-core@2221 50344849-fb0f-0410-a53f-a94597aac05c

---
+++ src/main/java/org/jboss/ws/tools/jaxws/spi/WebServiceImporterProvider.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ws.tools.jaxws.spi;
+
+import org.jboss.ws.tools.jaxws.api.WebServiceImporter;
+
+/**
+ * WebServiceImporterProvider defines the contract for a WebServiceImporter provider.
+ * 
+ * @author <a href="mailto:jason.greene@jboss.com">Jason T. Greene</a>
+ */
+public interface WebServiceImporterProvider
+{
+   /**
+    * Create a new WebServiceImporter. There are no restrictions on how this
+    * should be performed. 
+    * 
+    * @return a new WebServiceImporter
+    */
+   public WebServiceImporter createImporter();
+}
