0e65608fd85a0049736dbfa5e33d18a89723f5dc
heiko.braun@jboss.com <heiko.braun@jboss.com>
2007-06-06 13:43:55 +0000

    Fix JBWS-1563
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/trunk/jbossws-core@3472 50344849-fb0f-0410-a53f-a94597aac05c

--- src/main/java/org/jboss/ws/tools/wsdl/WSDLDefinitionsFactory.java
+++ src/main/java/org/jboss/ws/tools/wsdl/WSDLDefinitionsFactory.java
@@ -23,19 +23,6 @@ package org.jboss.ws.tools.wsdl;
 
 // $Id$
 
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.ConnectException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import javax.wsdl.Definition;
-import javax.wsdl.factory.WSDLFactory;
-import javax.wsdl.xml.WSDLReader;
-import javax.xml.parsers.DocumentBuilder;
-
 import org.jboss.logging.Logger;
 import org.jboss.ws.Constants;
 import org.jboss.ws.core.utils.JBossWSEntityResolver;
@@ -46,6 +33,18 @@ import org.jboss.wsf.spi.utils.DOMUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.EntityResolver;
+
+import javax.wsdl.Definition;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
+import javax.xml.parsers.DocumentBuilder;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.ConnectException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * A factory that creates a <code>WSDLDefinitions</code> object from an URL.
@@ -59,138 +58,137 @@ import org.xml.sax.EntityResolver;
  */
 public class WSDLDefinitionsFactory
 {
-   // provide logging
-   private static final Logger log = Logger.getLogger(WSDLDefinitionsFactory.class);
+	// provide logging
+	private static final Logger log = Logger.getLogger(WSDLDefinitionsFactory.class);
 
-   // This feature is set by default in wsdl4j, it means the object structure contains the imported arguments
-   public static final String FEATURE_IMPORT_DOCUMENTS = "javax.wsdl.importDocuments";
-   // Set this feature for additional debugging output
-   public static final String FEATURE_VERBOSE = "javax.wsdl.verbose";
+	// This feature is set by default in wsdl4j, it means the object structure contains the imported arguments
+	public static final String FEATURE_IMPORT_DOCUMENTS = "javax.wsdl.importDocuments";
+	// Set this feature for additional debugging output
+	public static final String FEATURE_VERBOSE = "javax.wsdl.verbose";
 
-   // The WSDL reader features
-   private Map features = new HashMap();
+	// The WSDL reader features
+	private Map features = new HashMap();
 
-   // Hide constructor
-   private WSDLDefinitionsFactory()
-   {
-   }
+	// Hide constructor
+	private WSDLDefinitionsFactory()
+	{
+	}
 
-   /**
-    * Create a new instance of a wsdl factory
-    */
-   public static WSDLDefinitionsFactory newInstance()
-   {
-      return new WSDLDefinitionsFactory();
-   }
+	/**
+	 * Create a new instance of a wsdl factory	 
+	 */
+	public static WSDLDefinitionsFactory newInstance()
+	{
+		return new WSDLDefinitionsFactory();
+	}
 
-   /**
-    * Set a feature on the underlying reader
-    */
-   public void setFeature(String name, boolean value) throws IllegalArgumentException
-   {
-      features.put(name, new Boolean(value));
-   }
+	/**
+	 * Set a feature on the underlying reader
+	 */
+	public void setFeature(String name, boolean value) throws IllegalArgumentException
+	{
+		features.put(name, new Boolean(value));
+	}
 
-   /**
-    * Read the wsdl document from the given URL
-    */
-   public WSDLDefinitions parse(URL wsdlLocation) throws WSDLException
-   {
-      if (wsdlLocation == null)
-         throw new IllegalArgumentException("URL cannot be null");
+	/**
+	 * Read the wsdl document from the given URL
+	 */
+	public WSDLDefinitions parse(URL wsdlLocation) throws WSDLException
+	{
+		if (wsdlLocation == null)
+			throw new IllegalArgumentException("URL cannot be null");
 
-      if(log.isDebugEnabled()) log.debug("parse: " + wsdlLocation.toExternalForm());
-      
-      EntityResolver entityResolver = new JBossWSEntityResolver();
-      WSDLDefinitions wsdlDefinitions = null;
-      try
-      {
-         Document wsdlDoc = getDocument(wsdlLocation);
-         String defaultNamespace = getDefaultNamespace(wsdlDoc);
-         if (Constants.NS_WSDL11.equals(defaultNamespace))
-         {
-            WSDLFactory wsdlFactory = WSDLFactory.newInstance();
-            WSDLReader wsdlReader = wsdlFactory.newWSDLReader();
-            wsdlReader.setFeature("javax.wsdl.verbose", false);
+		if(log.isDebugEnabled()) log.debug("parse: " + wsdlLocation.toExternalForm());
 
-            // Setup reader features
-            Iterator it = features.entrySet().iterator();
-            while (it.hasNext())
-            {
-               Map.Entry entry = (Map.Entry)it.next();
-               String key = (String)entry.getKey();
-               Boolean flag = (Boolean)entry.getValue();
-               wsdlReader.setFeature(key, flag.booleanValue());
-            }
+		EntityResolver entityResolver = new JBossWSEntityResolver();
+		WSDLDefinitions wsdlDefinitions = null;
+		try
+		{
+			Document wsdlDoc = getDocument(wsdlLocation);
+			String defaultNamespace = getDefaultNamespace(wsdlDoc);
+			if (Constants.NS_WSDL11.equals(defaultNamespace))
+			{
+				WSDLFactory wsdlFactory = WSDLFactory.newInstance(JBossWSDLFactoryImpl.class.getName());
+				WSDLReader wsdlReader = wsdlFactory.newWSDLReader();				
+				wsdlReader.setFeature("javax.wsdl.verbose", false);
 
-            // Set EntityResolver in patched version of wsdl4j-1.5.2jboss
-            // [TODO] show the usecase that needs this
-            //((WSDLReaderImpl)wsdlReader).setEntityResolver(entityResolver);            
+				// Setup reader features
+				Iterator it = features.entrySet().iterator();
+				
+				while (it.hasNext())
+				{
+					Map.Entry entry = (Map.Entry)it.next();
+					String key = (String)entry.getKey();
+					Boolean flag = (Boolean)entry.getValue();
+					wsdlReader.setFeature(key, flag.booleanValue());
+				}
 
-            Definition definition = wsdlReader.readWSDL(new WSDLLocatorImpl(entityResolver, wsdlLocation));
-            wsdlDefinitions = new WSDL11Reader().processDefinition(definition, wsdlLocation);
-            wsdlDefinitions.setWsdlDocument(wsdlDoc);
-         }
-         else
-            throw new WSDLException("Invalid default namespace: " + defaultNamespace);
+				Definition definition = wsdlReader.readWSDL(new WSDLLocatorImpl(entityResolver, wsdlLocation));
+				wsdlDefinitions = new WSDL11Reader().processDefinition(definition, wsdlLocation);
+				wsdlDefinitions.setWsdlDocument(wsdlDoc);
+			}
+			else
+			{
+				throw new WSDLException("Invalid default namespace: " + defaultNamespace);
+			}
+			
+			if (log.isTraceEnabled())
+			{
+				StringWriter stwr = new StringWriter();
+				WSDL11Writer wsdlWriter = new WSDL11Writer(wsdlDefinitions);
+				wsdlWriter.write(stwr);
+				log.trace("\n" + stwr.toString());
+			}
+		}
+		catch (WSDLException e)
+		{
+			throw e;
+		}
+		catch (RuntimeException rte)
+		{
+			throw rte;
+		}
+		catch (Exception e)
+		{
+			throw new WSDLException(e);
+		}
 
-         if (log.isTraceEnabled())
-         {
-            StringWriter stwr = new StringWriter();
-            WSDL11Writer wsdlWriter = new WSDL11Writer(wsdlDefinitions);
-            wsdlWriter.write(stwr);
-            log.trace("\n" + stwr.toString());
-         }
-      }
-      catch (WSDLException e)
-      {
-         throw e;
-      }
-      catch (RuntimeException rte)
-      {
-         throw rte;
-      }
-      catch (Exception e)
-      {
-         throw new WSDLException(e);
-      }
+		return wsdlDefinitions;
+	}
 
-      return wsdlDefinitions;
-   }
+	/** Get the WSDL document.
+	 */
+	private Document getDocument(URL wsdlLocation) throws WSDLException
+	{
+		try
+		{
+			InputStream inputStream = new ResourceURL(wsdlLocation).openStream();
+			try
+			{
+				DocumentBuilder builder = DOMUtils.getDocumentBuilder();
+				return builder.parse(inputStream);
+			}
+			finally
+			{
+				inputStream.close();
+			}
+		}
+		catch (ConnectException ex)
+		{
+			throw new WSDLException("Cannot connect to: " + wsdlLocation);
+		}
+		catch (Exception ex)
+		{
+			throw new WSDLException("Cannot parse wsdlLocation: " + wsdlLocation, ex);
+		}
+	}
 
-   /** Get the WSDL document.
-    */
-   private Document getDocument(URL wsdlLocation) throws WSDLException
-   {
-      try
-      {
-         InputStream inputStream = new ResourceURL(wsdlLocation).openStream();
-         try
-         {
-            DocumentBuilder builder = DOMUtils.getDocumentBuilder();
-            return builder.parse(inputStream);
-         }
-         finally
-         {
-            inputStream.close();
-         }
-      }
-      catch (ConnectException ex)
-      {
-         throw new WSDLException("Cannot connect to: " + wsdlLocation);
-      }
-      catch (Exception ex)
-      {
-         throw new WSDLException("Cannot parse wsdlLocation: " + wsdlLocation, ex);
-      }
-   }
-
-   /** Get the default namespace for the given WSDL
-    */
-   private String getDefaultNamespace(Document wsdlDoc) throws WSDLException
-   {
-      Element root = wsdlDoc.getDocumentElement();
-      String defaultNamespace = root.getNamespaceURI();
-      return defaultNamespace;
-   }
+	/** Get the default namespace for the given WSDL
+	 */
+	private String getDefaultNamespace(Document wsdlDoc) throws WSDLException
+	{
+		Element root = wsdlDoc.getDocumentElement();
+		String defaultNamespace = root.getNamespaceURI();
+		return defaultNamespace;
+	}
 }
