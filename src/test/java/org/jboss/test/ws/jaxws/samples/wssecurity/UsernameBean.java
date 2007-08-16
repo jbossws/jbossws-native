c274080f44926c28bf527adba5eeb483de0ca44b
thomas.diesler@jboss.com <thomas.diesler@jboss.com>
2007-08-16 17:27:54 +0000

    [JBWS-1766] UsernameToken ignores BindingProvider.USERNAME_PROPERTY
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/native-2.0@4392 50344849-fb0f-0410-a53f-a94597aac05c

--- src/test/java/org/jboss/test/ws/jaxws/samples/wssecurity/UsernameBean.java
+++ src/test/java/org/jboss/test/ws/jaxws/samples/wssecurity/UsernameBean.java
@@ -21,41 +21,26 @@
  */
 package org.jboss.test.ws.jaxws.samples.wssecurity;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Iterator;
+// $Id$
+
+import java.security.Principal;
 
 import javax.annotation.Resource;
 import javax.jws.WebMethod;
-import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPEnvelope;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPHeader;
-import javax.xml.soap.SOAPMessage;
-import javax.xml.soap.SOAPPart;
-import javax.xml.ws.handler.MessageContext;
-import javax.xml.ws.handler.soap.SOAPMessageContext;
 import javax.xml.ws.WebServiceContext;
 
 import org.jboss.logging.Logger;
 import org.jboss.ws.annotation.EndpointConfig;
-import org.jboss.ws.extensions.security.Constants;
-import org.jboss.ws.extensions.security.Util;
-import org.jboss.ws.core.soap.SOAPElementImpl;
-import org.jboss.ws.core.soap.SOAPEnvelopeImpl;
-import org.jboss.ws.core.soap.SOAPHeaderImpl;
-import org.w3c.dom.Element;
 
 /**
  * The SEI implementation used by the SimpleUsernameTestCase
  *
  * @author <a href="mailto:mageshbk@jboss.com">Magesh Kumar B</a>
+ * @author Thomas.Diesler@jboss.com
  * @since 15-Aug-2007
- * @version $Revision$ 
  */
 @WebService(name = "UsernameEndpoint", serviceName = "UsernameService", targetNamespace = "http://org.jboss.ws/samples/wssecurity")
 @EndpointConfig(configName = "Standard WSSecurity Endpoint")
@@ -63,46 +48,16 @@ import org.w3c.dom.Element;
 public class UsernameBean
 {
    private Logger log = Logger.getLogger(UsernameBean.class);
-   
+
    @Resource
    WebServiceContext wsCtx;
 
    @WebMethod
+   @WebResult(partName = "return")
    public String getUsernameToken()
    {
-      String retObj = "";
-      try
-      {       
-         MessageContext jaxwsContext = (MessageContext)wsCtx.getMessageContext();
-         SOAPMessage soapMessage = ((SOAPMessageContext)jaxwsContext).getMessage();
-         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-         soapMessage.writeTo(stream);
-         log.info(stream.toString());
-         
-         SOAPPart soapPart = soapMessage.getSOAPPart();
-         SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
-         StringWriter strw = new StringWriter();
-         ((SOAPEnvelopeImpl)soapEnvelope).writeElement(strw);
-         log.info(strw.toString());
-
-         SOAPHeader soapHeader = soapEnvelope.getHeader();
-         strw = new StringWriter();
-         ((SOAPHeaderImpl)soapHeader).writeElement(strw);
-         retObj = strw.toString();
-         log.info(retObj);
-
-         QName secQName = new QName(Constants.WSSE_NS, "Security");
-         Element secHeaderElement = Util.findElement(soapHeader, secQName);
-         log.info(secHeaderElement);
-      }
-      catch (SOAPException se)
-      {
-         log.error(se.getMessage());
-      }
-      catch (IOException ioe)
-      {
-         log.error(ioe.getMessage());
-      }
-      return retObj;
+      Principal principal = wsCtx.getUserPrincipal();
+      log.info("getUsernameToken: " + principal);
+      return principal.toString();
    }
 }
