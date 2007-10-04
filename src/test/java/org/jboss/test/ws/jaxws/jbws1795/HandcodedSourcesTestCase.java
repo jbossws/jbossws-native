c6483de85e4292cd04b72999359e1d47b583cb2b
richard.opalka@jboss.com <richard.opalka@jboss.com>
2007-10-04 13:35:49 +0000

    compatibility with our convention
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@4678 50344849-fb0f-0410-a53f-a94597aac05c

--- src/test/java/org/jboss/test/ws/jaxws/jbws1795/HandcodedSourcesTestCase.java
+++ src/test/java/org/jboss/test/ws/jaxws/jbws1795/HandcodedSourcesTestCase.java
@@ -1,3 +1,24 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.jboss.test.ws.jaxws.jbws1795;
 
 import java.net.URL;
@@ -18,6 +39,15 @@ import org.jboss.test.ws.jaxws.jbws1795.service.TravelAgentEndpoint;
 import org.jboss.wsf.test.JBossWSTest;
 import org.jboss.wsf.test.JBossWSTestSetup;
 
+/**
+ * [JBWS-1795] Type Substitution doesn't work with Schema2Java Client approach
+ * These test sources were provided by our users. 
+ *
+ * http://jira.jboss.org/jira/browse/JBWS-1795
+ *
+ * @author richard.opalka@jboss.com
+ * @since 3-Oct-2007
+ */
 public class HandcodedSourcesTestCase extends JBossWSTest
 {
    
