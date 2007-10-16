c4ca673eaefadf214f803eb1c726d66f6cd24737
thomas.diesler@jboss.com <thomas.diesler@jboss.com>
2007-10-16 13:50:20 +0000

    [JBWS-1798] JBossWS cannot find local schema with relative urls
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@4776 50344849-fb0f-0410-a53f-a94597aac05c

---
+++ src/test/java/org/jboss/test/ws/jaxws/jbws1798/generated/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://jbws1798.jaxws.ws.test.jboss.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.jboss.test.ws.jaxws.jbws1798.generated;
