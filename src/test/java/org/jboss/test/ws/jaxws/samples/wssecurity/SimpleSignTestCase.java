95e9a5e811a14361d8ac21202ac8aef483edb41e
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2008-02-28 14:42:59 +0000

    [JBWS-2011] Adding test case
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@5849 50344849-fb0f-0410-a53f-a94597aac05c

--- src/test/java/org/jboss/test/ws/jaxws/samples/wssecurity/SimpleSignTestCase.java
+++ src/test/java/org/jboss/test/ws/jaxws/samples/wssecurity/SimpleSignTestCase.java
@@ -54,11 +54,23 @@ public class SimpleSignTestCase extends JBossWSTest
    public void testEndpoint() throws Exception
    {
       Hello hello = getPort();
-
+      performTest(hello, "Kermit");
+   }
+   
+   public void testEndpointWithCRLFs() throws Exception
+   {
+      Hello hello = getPort();
+      performTest(hello, "Kermit\nmy friend");
+      performTest(hello, "Kermit\rmy friend");
+      performTest(hello, "Kermit\r\nmy friend");
+   }
+   
+   private void performTest(Hello hello, String msg) throws Exception
+   {
       UserType in0 = new UserType();
-      in0.setMsg("Kermit");
+      in0.setMsg(msg);
       UserType retObj = hello.echoUserType(in0);
-      assertEquals("Kermit", retObj.getMsg());
+      assertEquals(msg, retObj.getMsg());
    }
 
    private Hello getPort() throws Exception
