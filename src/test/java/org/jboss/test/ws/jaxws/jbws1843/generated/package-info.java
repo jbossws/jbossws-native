58253d7ce95d7713d622f1151bdd091e55c46295
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com>
2008-05-20 15:12:52 +0000

    [JBPAPP-849] WSDL with custom schema imports causes java.lang.OutOfMemoryError: Java heap space when there are circular schema imports dependencies.
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/jbossws-native-2.0.1.SP2_CP@7114 50344849-fb0f-0410-a53f-a94597aac05c

---
+++ src/test/java/org/jboss/test/ws/jaxws/jbws1843/generated/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://jbws1843.jaxws.ws.test.jboss.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.jboss.test.ws.jaxws.jbws1843.generated;
