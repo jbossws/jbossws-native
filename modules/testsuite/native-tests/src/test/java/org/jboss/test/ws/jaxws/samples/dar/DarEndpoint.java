878169a1bc2ef2ae34c59fdc4474540adc913fb5
thomas.diesler@jboss.com <thomas.diesler@jboss.com>
2008-05-30 15:38:52 +0000

    [JBWS-2107] Resolve dependency on @SecurityDomain
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@7240 50344849-fb0f-0410-a53f-a94597aac05c

--- modules/testsuite/native-tests/src/test/java/org/jboss/test/ws/jaxws/samples/dar/DarEndpoint.java
+++ modules/testsuite/native-tests/src/test/java/org/jboss/test/ws/jaxws/samples/dar/DarEndpoint.java
@@ -28,7 +28,6 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
-import org.jboss.annotation.security.SecurityDomain;
 import org.jboss.wsf.spi.annotation.WebContext;
 
 /**
