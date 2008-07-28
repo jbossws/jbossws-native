6ba3adfb2207fe32f0e25894368469120473fa1a
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2008-07-28 07:40:18 +0000

    [JBWS-2265] Fixing namespace
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@7917 50344849-fb0f-0410-a53f-a94597aac05c

--- modules/core/src/main/java/org/jboss/ws/extensions/security/Constants.java
+++ modules/core/src/main/java/org/jboss/ws/extensions/security/Constants.java
@@ -58,9 +58,9 @@ public class Constants
 
    public static final String BASE64_ENCODING_TYPE = WSS_SOAP_NS + "#Base64Binary";
    
-   public static final String PASSWORD_TEXT_TYPE = WSSE_NS + "#PasswordText";
+   public static final String PASSWORD_TEXT_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0#PasswordText";
    
-   public static final String PASSWORD_DIGEST_TYPE = WSSE_NS + "#PasswordDigest";
+   public static final String PASSWORD_DIGEST_TYPE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0#PasswordDigest";
 
    public static final String WSSE_HEADER = WSSE_PREFIX + ":Security";
 
