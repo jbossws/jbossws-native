818ec7f6c2da7f94912717137b4f3ba0c5d44ee2
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com>
2008-11-07 12:06:04 +0000

    [JBWS-2372] Protocol not considered when re-writing address in WSDL based on request address.
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@8671 50344849-fb0f-0410-a53f-a94597aac05c

--- modules/core/src/main/java/org/jboss/ws/core/server/WSDLRequestHandler.java
+++ modules/core/src/main/java/org/jboss/ws/core/server/WSDLRequestHandler.java
@@ -184,7 +184,6 @@ public class WSDLRequestHandler
                   String orgLocation = locationAttr.getNodeValue();
 
                   URL orgURL = new URL(orgLocation);
-                  String orgProtocol = orgURL.getProtocol();
                   String orgHost = orgURL.getHost();
                   int orgPort = orgURL.getPort();
                   String orgPath = orgURL.getPath();
@@ -192,10 +191,11 @@ public class WSDLRequestHandler
                   if (ServerConfig.UNDEFINED_HOSTNAME.equals(orgHost))
                   {
                      URL newURL = new URL(wsdlHost); 
+                     String newProtocol = newURL.getProtocol();
                      String newHost = newURL.getHost();
                      int newPort = newURL.getPort();
                      
-                     String newLocation = orgProtocol + "://" + newHost;
+                     String newLocation = newProtocol + "://" + newHost;
                      if (newPort != -1)
                         newLocation += ":" + newPort;
                      else if (orgPort != -1)
