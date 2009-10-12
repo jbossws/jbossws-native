e0ee910fe0cb6759aa0e4e36cffaa6dbf5a24d2c
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2009-10-12 17:04:17 +0000

    [JBWS-2784] Adding testcase
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@10895 50344849-fb0f-0410-a53f-a94597aac05c

---
+++ modules/testsuite/native-tests/src/test/java/org/jboss/test/ws/jaxws/jbws2784/JBWS2784TestCase.java
@@ -0,0 +1,45 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.ws.jaxws.jbws2784;
+
+import junit.framework.Test;
+
+import org.jboss.wsf.test.JBossWSTest;
+import org.jboss.wsf.test.JBossWSTestSetup;
+
+/**
+ * [JBWS-2784] MetaDataBuilder chokes on multiple imports
+ * 
+ * @author alessio.soldano@jboss.com
+ */
+public class JBWS2784TestCase extends JBossWSTest
+{
+   public static Test suite()
+   {
+      return new JBossWSTestSetup(JBWS2784TestCase.class, "jaxws-jbws2784.jar");
+   }
+                                     
+   public void test() throws Exception
+   {
+      //nothing to do for this test once the deploy succeeded
+   }   
+}
