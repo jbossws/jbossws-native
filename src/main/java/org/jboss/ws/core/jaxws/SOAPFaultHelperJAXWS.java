9c5f4bf3ae17ea6f77ec242a76f6b994d24031bf
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com>
2009-11-29 16:18:32 +0000

    [JBPAPP-3178] Missing fault detail in SOAPFault.
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/jbossws-native-2.0.1.SP2_CP@11167 50344849-fb0f-0410-a53f-a94597aac05c

--- src/main/java/org/jboss/ws/core/jaxws/SOAPFaultHelperJAXWS.java
+++ src/main/java/org/jboss/ws/core/jaxws/SOAPFaultHelperJAXWS.java
@@ -72,6 +72,10 @@ public class SOAPFaultHelperJAXWS
    /** Factory method for FaultException for a given SOAPFault */
    public static SOAPFaultException getSOAPFaultException(SOAPFault soapFault)
    {
+
+      if (soapFault == null)
+         throw new IllegalArgumentException("SOAPFault cannot be null");
+
       SOAPFaultException faultEx = new SOAPFaultException(soapFault);
 
       Detail detail = soapFault.getDetail();
@@ -155,9 +159,19 @@ public class SOAPFaultHelperJAXWS
       try
       {
          SOAPMessageImpl faultMessage;
+         Throwable cause = reqEx.getCause();
          if (reqEx instanceof SOAPFaultException)
          {
             faultMessage = toSOAPMessage((SOAPFaultException)reqEx);
+         }
+         /* JAX-WS 6.4.1: When an implementation catches an exception thrown by a
+          * service endpoint implementation and the cause of that exception is an
+          * instance of the appropriate ProtocolException subclass for the protocol
+          * in use, an implementation MUST reflect the information contained in the
+          * ProtocolException subclass within the generated protocol level fault. */
+         else if (cause != null && cause instanceof SOAPFaultException)
+         {
+            faultMessage = toSOAPMessage((SOAPFaultException)cause);
          }
          else if (reqEx instanceof CommonSOAPFaultException)
          {
@@ -237,23 +251,8 @@ public class SOAPFaultHelperJAXWS
       SOAPMessageImpl soapMessage = createSOAPMessage();
 
       SOAPBody soapBody = soapMessage.getSOAPBody();
-      SOAPFault soapFault;
 
-      /* JAX-WS 6.4.1: When an implementation catches an exception thrown by a
-       * service endpoint implementation and the cause of that exception is an
-       * instance of the appropriate ProtocolException subclass for the protocol
-       * in use, an implementation MUST reflect the information contained in the
-       * ProtocolException subclass within the generated protocol level fault. */
-      Throwable cause = ex.getCause();
-      if (cause instanceof SOAPFaultException)
-      {
-         populateSOAPFault(soapBody, (SOAPFaultException)cause);
-         soapFault = soapBody.getFault();
-      }
-      else
-      {
-         soapFault = soapBody.addFault(getFallbackFaultCode(), getFallbackFaultString(ex));
-      }
+      SOAPFault soapFault = soapBody.addFault(getFallbackFaultCode(), getFallbackFaultString(ex));
 
       CommonMessageContext msgContext = MessageContextAssociation.peekMessageContext();
       SerializationContext serContext = msgContext.getSerializationContext();
