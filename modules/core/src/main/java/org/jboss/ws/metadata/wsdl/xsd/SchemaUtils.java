67d2eca932d53ae55117106a6b50b9fdc51d9420
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com>
2010-01-13 17:52:03 +0000

    [JBWS-2845] NullPointerException deploying JAX-WS endpoint with types in target namespace ending with '#'
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@11402 50344849-fb0f-0410-a53f-a94597aac05c

--- modules/core/src/main/java/org/jboss/ws/metadata/wsdl/xsd/SchemaUtils.java
+++ modules/core/src/main/java/org/jboss/ws/metadata/wsdl/xsd/SchemaUtils.java
@@ -535,6 +535,7 @@ public class SchemaUtils
       fname = fname.replace('/', '_');
       fname = fname.replace(':', '_');
       fname = fname.replace('?', '_');
+      fname = fname.replace('#', '_');
       
       return File.createTempFile("JBossWS_" + fname, ".xsd", tmpdir);
    }
