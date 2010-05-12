771280863832ca34945cb1b5f237b47ad0e195a3
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com>
2010-05-12 16:20:01 +0000

    [JBPAPP-4120] JAX-WS Port injected using @WebServiceRef retains last message.
    Refactoring to eliminate caching of EndpointInvocation within the CommonClient
    referenced when injected.
    
    
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/jbossws-native-2.0.1.SP2_CP@12219 50344849-fb0f-0410-a53f-a94597aac05c

--- src/main/java/org/jboss/ws/core/CommonClient.java
+++ src/main/java/org/jboss/ws/core/CommonClient.java
@@ -84,8 +84,6 @@ public abstract class CommonClient implements StubExt, HeaderSource
    protected EndpointMetaData epMetaData;
    // The current operation name
    protected QName operationName;
-   // Output parameters
-   protected EndpointInvocation epInv;
    // The binding provider
    protected CommonBindingProvider bindingProvider;
    // A Map<QName,UnboundHeader> of header entries
@@ -239,6 +237,11 @@ public abstract class CommonClient implements StubExt, HeaderSource
 
    protected abstract boolean shouldMaintainSession();
 
+   protected EndpointInvocation createEndpointInvocation(OperationMetaData opMetaData)
+   {
+      return new EndpointInvocation(opMetaData);
+   }
+   
    /** Call invokation goes as follows:
     *
     * 1) synchronize the operation name with the operation meta data
@@ -279,7 +282,7 @@ public abstract class CommonClient implements StubExt, HeaderSource
          binding.setHeaderSource(this);
 
          // Create the invocation and sync the input parameters
-         epInv = new EndpointInvocation(opMetaData);
+         EndpointInvocation epInv = createEndpointInvocation(opMetaData);
          epInv.initInputParams(inputParams);
 
          // Set the required outbound properties
