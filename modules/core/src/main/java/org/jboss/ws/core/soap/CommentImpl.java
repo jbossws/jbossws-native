6f42130d6c232841482b17081f73432281e73034
darran.lofthouse@jboss.com <darran.lofthouse@jboss.com>
2010-05-19 15:53:59 +0000

    [JBPAPP-3960] org.jboss.ws.core.soap.TextImpl does not implement org.w3c.dom.Comment.
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/jbossws-native-3.1.2@12277 50344849-fb0f-0410-a53f-a94597aac05c

---
+++ modules/core/src/main/java/org/jboss/ws/core/soap/CommentImpl.java
@@ -0,0 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.ws.core.soap;
+
+/**
+ * A representation of a node whose value is text. A Text
+ * object may represent text that is content or text that is a comment.
+ *
+ */
+public class CommentImpl extends TextImpl implements org.w3c.dom.Comment
+{
+
+   public CommentImpl(org.w3c.dom.Node node)
+   {
+      super(node);
+   }
+
+}
