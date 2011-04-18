2c164d27b7e74386ecf985b9e2ae4838834867c9
richard.opalka@jboss.com <richard.opalka@jboss.com>
2011-04-18 15:02:02 +0000

    [JBWS-3273] fixing class collision with new jbossws-tools-common.jar module
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/trunk@14156 50344849-fb0f-0410-a53f-a94597aac05c

--- modules/core/src/main/java/org/jboss/ws/tools/ant/wstools.java
+++ modules/core/src/main/java/org/jboss/ws/tools/ant/wstools.java
@@ -21,6 +21,11 @@
  */
 package org.jboss.ws.tools.ant;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.MatchingTask;
 import org.apache.tools.ant.types.Path;
@@ -95,8 +100,8 @@ public class wstools extends MatchingTask
 
    public void execute() throws BuildException
    {
-      ClassLoader prevCL = SecurityActions.getContextClassLoader();
-      SecurityActions.setContextClassLoader(this.getClass().getClassLoader());
+      ClassLoader prevCL = getContextClassLoader();
+      setContextClassLoader(this.getClass().getClassLoader());
       try
       {
          String[] args = new String[] { "-dest", dest, "-config", config };
@@ -116,7 +121,77 @@ public class wstools extends MatchingTask
       }
       finally
       {
-         SecurityActions.setContextClassLoader(prevCL);
+         setContextClassLoader(prevCL);
+      }
+   }
+
+   private static ClassLoader getContextClassLoader()
+   {
+      SecurityManager sm = System.getSecurityManager();
+      if (sm == null)
+      {
+         return Thread.currentThread().getContextClassLoader();
+      }
+      else
+      {
+         return AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
+            public ClassLoader run()
+            {
+               return Thread.currentThread().getContextClassLoader();
+            }
+         });
+      }
+   }
+
+   /**
+    * Set context classloader.
+    *
+    * @param cl the classloader
+    * @return previous context classloader
+    * @throws Throwable for any error
+    */
+   private static ClassLoader setContextClassLoader(final ClassLoader cl)
+   {
+      if (System.getSecurityManager() == null)
+      {
+         ClassLoader result = Thread.currentThread().getContextClassLoader();
+         if (cl != null)
+            Thread.currentThread().setContextClassLoader(cl);
+         return result;
+      }
+      else
+      {
+         try
+         {
+            return AccessController.doPrivileged(new PrivilegedExceptionAction<ClassLoader>() {
+               public ClassLoader run() throws Exception
+               {
+                  try
+                  {
+                     ClassLoader result = Thread.currentThread().getContextClassLoader();
+                     if (cl != null)
+                        Thread.currentThread().setContextClassLoader(cl);
+                     return result;
+                  }
+                  catch (Exception e)
+                  {
+                     throw e;
+                  }
+                  catch (Error e)
+                  {
+                     throw e;
+                  }
+                  catch (Throwable e)
+                  {
+                     throw new RuntimeException("Error setting context classloader", e);
+                  }
+               }
+            });
+         }
+         catch (PrivilegedActionException e)
+         {
+            throw new RuntimeException("Error running privileged action", e.getCause());
+         }
       }
    }
 }
