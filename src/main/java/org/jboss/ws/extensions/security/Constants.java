36b2f559db762315580726190ef7a4cbaddb02e4
jim.ma <jim.ma>
2011-05-18 10:56:36 +0000

    [JBPAPP-5827]:updated WSSE namespace
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/jbossws-native-2.0.1.SP2_CP@14399 50344849-fb0f-0410-a53f-a94597aac05c

--- src/main/java/org/jboss/ws/extensions/security/Constants.java
+++ src/main/java/org/jboss/ws/extensions/security/Constants.java
@@ -40,7 +40,8 @@ public class Constants
 
    public static final String WSSE_PREFIX = "wsse";
 
-   public static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+   public static final String WSSE_NS = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0";
+                                         
 
    public static final String WSU_PREFIX = "wsu";
 
