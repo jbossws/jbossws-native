2fd2fd8910e15b143cb7bb7bb312d596412c399a
alessio.soldano@jboss.com <alessio.soldano@jboss.com>
2012-03-02 16:34:12 +0000

    Merged revisions 15829 via svnmerge from https://svn.jboss.org/repos/jbossws/stack/native/trunk
    ........
      r15829 | alessio.soldano@jboss.com | 2012-03-02 17:29:42 +0100 (Fri, 02 Mar 2012) | 2 lines
      
      [JBWS-3439] Move JBossWS-Native stack too to Santuario 1.5.1
    ........
    
    
    git-svn-id: https://anonsvn.jboss.org/repos/jbossws/stack/native/branches/jbossws-native-4.0.x@15830 50344849-fb0f-0410-a53f-a94597aac05c

--- modules/core/src/main/java/org/jboss/ws/extensions/security/STRTransform.java
+++ modules/core/src/main/java/org/jboss/ws/extensions/security/STRTransform.java
@@ -22,6 +22,7 @@
 package org.jboss.ws.extensions.security;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ResourceBundle;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -91,9 +92,15 @@ public class STRTransform extends TransformSpi
       return STR_URI;
    }
 
+   @Override
+   protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input, Transform transformObject) throws IOException, CanonicalizationException,
+         InvalidCanonicalizerException, TransformationException, ParserConfigurationException, SAXException
+   {
+      return enginePerformTransform(input, null, transformObject);
+   }
 
    @Override
-   protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input) throws IOException, CanonicalizationException,
+   protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input, OutputStream os, Transform transformObject) throws IOException, CanonicalizationException,
          InvalidCanonicalizerException, TransformationException, ParserConfigurationException, SAXException
    {
 
@@ -117,7 +124,7 @@ public class STRTransform extends TransformSpi
          element = token.getSTRTransformElement();
 
          // Obtain the canonicalizer specified in the transformation parameters
-         Element parameters = XMLUtils.selectNode(this._transformObject.getElement().getFirstChild(), Constants.WSSE_NS,
+         Element parameters = XMLUtils.selectNode(transformObject.getElement().getFirstChild(), Constants.WSSE_NS,
                "TransformationParameters", 0);
          if (parameters == null)
             throw new TransformationException(BundleUtils.getMessage(bundle, "TRANSFORMATIONPARAMETERS_EXPECTED"));
